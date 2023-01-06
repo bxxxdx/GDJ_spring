27e8dadfe12d8fca019a076ad3ed59f1fbb0f9ea
bxxxdx <qudeh911@gmail.com>
2023-01-06 16:19:22 +0900

    2022-01-06 password 암호화, Model 객체로 session 관리하기

--- HelloSpring/src/main/java/com/bs/spring/member/controller/MemberController.java
+++ HelloSpring/src/main/java/com/bs/spring/member/controller/MemberController.java
@@ -1,26 +1,30 @@
 package com.bs.spring.member.controller;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.bs.spring.member.model.service.MemberService;
 import com.bs.spring.member.model.vo.Member;
 
 @Controller
+@SessionAttributes({"loginMember"})
 @RequestMapping("/member") //이 컨트롤러에 들어왔을 때 기본적으로 prefix로 /member/가 붙는다
 public class MemberController {
 	
 	
 	private MemberService service;
+	private BCryptPasswordEncoder passwordEncoder;
 	
 	@Autowired
-	public MemberController(MemberService service) {
+	public MemberController(MemberService service, BCryptPasswordEncoder passwordEncoder) {
 		this.service=service;
+		this.passwordEncoder=passwordEncoder;
 	}
 	
 //	@RequestMapping("/test/")
@@ -32,14 +36,17 @@ public class MemberController {
 	@RequestMapping("/loginMember.do")
 	//public String login(String userId, String password) {
 	//public String login(@RequestParam Map param){
-	public String loginMember(Member m, HttpSession session) {	
+	//public String loginMember(Member m, HttpSession session) {
+	//model은 request와 생명주기가 같지만 addAtribute를 사용하면 session과 생명주기가 같아질 수 있다.
+	public String loginMember(Member m, Model model) {	
 		Member loginMember = service.selectMemberById(m);
 		
-		//System.out.println(loginMember);
-		
-		if(loginMember != null && loginMember.getPassword().equals(m.getPassword())) {
+		//암호화된 패스워드를 원본값으링 비교하기 위해서는 BCryptPasswordEncoder 클래스가 제공하는 메소드를 이용해서 동등비교를 해야한다.
+		//matches("원본값","암호화값") 메소드를 이용
+		if(loginMember != null && passwordEncoder.matches(m.getPassword(), loginMember.getPassword())) {
 			//로그인 성공
-			session.setAttribute("loginMember", loginMember);
+			//session.setAttribute("loginMember", loginMember);
+			model.addAttribute("loginMember",loginMember);
 		}
 		
 		return "redirect:/";
@@ -47,9 +54,12 @@ public class MemberController {
 	
 	
 	@RequestMapping("/logoutMember.do")
-	public String logoutMember(HttpSession session) {
-		
-		session.invalidate();
+	//public String logoutMember(HttpSession session) {
+	public String logoutMember(SessionStatus session) {	
+		//session.invalidate();
+		if(!session.isComplete()) { //세션을 확인
+			session.setComplete(); //세션을 삭제
+		} 
 		
 		return "redirect:/";
 	}
@@ -62,6 +72,10 @@ public class MemberController {
 	@RequestMapping("/enrollMemberEnd.do")
 	public ModelAndView enrollMemberEnd(ModelAndView mv, Member m) {
 		
+		//password 암호화 처리하기
+		String encodePassword = passwordEncoder.encode(m.getPassword());
+		m.setPassword(encodePassword);
+			
 		int result = service.insertMember(m);
 		
 		String msg = "";
