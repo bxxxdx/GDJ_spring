e87d34ea500120e96ae57440cf7ff1088cc5aeee
bxxxdx <qudeh911@gmail.com>
2023-01-16 12:05:22 +0900

    2023-01-16 [BD] JPA 설정 중간저장

---
+++ HelloSpring/src/main/java/com/bs/spring/jpa/config/JpaConfig.java
@@ -0,0 +1,43 @@
+package com.bs.spring.jpa.config;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JpaConfig {
+	//jpa로 DB와 클래스를 연결하기 위해서는
+	//jpa가 제공하는 EntityManager, EntityTransaction 클래스를 이용한다.
+	//Persistence 클래스에서 제공하는 createEntityManagerFactory() 메소드를 이용해서 
+	//EntityManagerFactory 클래스를 생성
+	//EntityManagerFactory 클래스의 createEntityManager() 메소드를 이용해서
+	//EntityManager 클래스 생성
+	
+	//생성해야할 객체들을 Bean으로 등록하기
+	
+	@Bean
+	public EntityManagerFactory entityManagerFactory() {
+		//persistence에 설정한 persistence-unit name값으로 연결할 db를 설정해서 생성함.
+		//createEntityManagerFactory 메소드의 인자로 대입
+		//Factory같은 경우 비용이 비싸서 웬만하면 static, final, singleton으로 만들기
+		//EntityManager는 Spring 전용이 아니라 Eclipse에서도 사용할 수 있음
+		return Persistence.createEntityManagerFactory("bstest");
+	}
+	
+	@Bean
+	public EntityManager entityManager() {
+		return entityManagerFactory().createEntityManager();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
